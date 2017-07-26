d208ce6818bfe9ba4d54b8e9f11539cca54abd9f
Vincent Martorano <martoranov94@gmail.com>
2017-07-26 16:41:05 -0400

    missing projects formating and pages and heroku

---
+++ Portfolio/app/controllers/home_controller.rb
@@ -0,0 +1,37 @@
+class HomeController < ApplicationController
+
+  skip_before_filter  :verify_authenticity_token
+
+  def index
+  end
+
+  def email
+
+    # set the from, subject and to addresses
+    from = SendGrid::Email.new(email: params[:from])
+    subject = params[:subject]
+    to = SendGrid::Email.new(email: "martoranov94@gmail.com")
+
+
+    # set the content to send in the email
+    content = SendGrid::Content.new(type: 'text/plain', value: params[:content])
+
+    # set the mail attribute values
+    mail = SendGrid::Mail.new(from, subject, to, content)
+
+    # pass in the sendgrid api key
+    sg = SendGrid::API.new(api_key: ENV["SENDGRID_API_KEY"])
+
+    # send the email
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+    # display the response status code and body
+    puts "Status code = #{response.status_code}"
+    puts response.body
+
+    puts "MESAGE SAKJFSKDFSADFNSAJKDFSAD F SENT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+    redirect_to root_path
+  end
+
+end
